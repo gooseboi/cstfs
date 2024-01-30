997afa3aa363c52811968360cd0a2dfaa7725e51
Guzman Zugnoni <gooseiman@protonmail.com>
2024-01-30 15:45:03 -0300

    provide output when initializing db

--- src/init.rs
+++ src/init.rs
@@ -2,6 +2,7 @@ use std::time::Instant;
 
 use camino::Utf8Path;
 use color_eyre::{eyre::WrapErr, Result};
+use crossterm::{cursor::MoveToColumn, QueueableCommand};
 
 use crate::db;
 use crate::utils::{hash_file, recursive_directory_read};
@@ -14,9 +15,17 @@ pub fn init(data_path: &Utf8Path) -> Result<()> {
         .wrap_err("Failed creating insert transaction")?;
     println!("Starting database generation at \"{data_path}\"");
     let now = Instant::now();
-    for p in
-        recursive_directory_read(data_path).wrap_err("Failed reading data directory contents")?
+    let directory_contents =
+        recursive_directory_read(data_path).wrap_err("Failed reading data directory contents")?;
+    let total = directory_contents.len();
+    for (i, p) in directory_contents
+        .iter()
+        .enumerate()
+        .map(|(i, p)| (i + 1, p))
     {
+        let mut stdout = std::io::stdout();
+        stdout.queue(MoveToColumn(0)).wrap_err("Failed to move cursor to beginning of line")?;
+        println!("Adding file {i}/{total}...");
         let h = hash_file(&p).wrap_err_with(|| format!("Could not hash file {p}"))?;
         let p = p
             .strip_prefix(data_path)
