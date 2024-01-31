0ab70997754343bfd1105ddda3d0d792dd9717ab
Guzman Zugnoni <gooseiman@protonmail.com>
2024-01-31 07:34:34 -0300

    handle duplicate files when initing

--- src/db.rs
+++ src/db.rs
@@ -4,13 +4,13 @@ use rusqlite::{Connection, Transaction};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("database could not be opened")]
+    #[error("database could not be opened:\n{0}")]
     Open(rusqlite::Error),
 
-    #[error("migrations could not be performed on database")]
+    #[error("migrations could not be performed on database:\n{0}")]
     Migration(rusqlite::Error),
 
-    #[error("{path}:{hash} could not be inserted into database")]
+    #[error("{path}:{hash} could not be inserted into database:\n{source}")]
     InsertionFailure {
         path: Utf8PathBuf,
         hash: String,
@@ -23,16 +23,41 @@ pub enum Error {
         path_old: Utf8PathBuf,
     },
 
-    #[error("fetch failure")]
+    #[error("fetch failure:\n{0}")]
     QueryFailure(rusqlite::Error),
 
-    #[error("unknown db error")]
+    #[error("update failure:\n{0}")]
+    UpdateFailure(rusqlite::Error),
+
+    #[error("duplicate paths found on query: {0:?}")]
+    DuplicatePaths(Vec<Utf8PathBuf>),
+
+    #[error("cannot update path of hash {0} which does not exist")]
+    HashDoesNotExist(String),
+
+    #[error("query affected {count} rows, expected {min_rows}..{max_rows}: {msg}")]
+    TooManyRowsAffected {
+        count: usize,
+        min_rows: usize,
+        max_rows: usize,
+        msg: String,
+    },
+
+    #[error("query affected {count} rows, expected {min_rows}..{max_rows}: {msg}")]
+    TooFewRowsAffected {
+        count: usize,
+        min_rows: usize,
+        max_rows: usize,
+        msg: String,
+    },
+
+    #[error("unknown db error:\n{0}")]
     Unknown(#[from] color_eyre::Report),
 }
 
 pub fn open(data_path: &Utf8Path) -> Result<Connection, Error> {
     let db_path = path(data_path);
-    let conn = Connection::open(&db_path).map_err(Error::Open)?;
+    let conn = Connection::open(db_path).map_err(Error::Open)?;
 
     conn.execute(
         "
@@ -57,7 +82,7 @@ pub fn insert_into(
     hash: &str,
 ) -> Result<(), Error> {
     let select_result: Result<String, rusqlite::Error> = transaction.query_row(
-        "SELECT path, hash FROM files WHERE hash = ?1",
+        "SELECT path FROM files as f WHERE f.hash = ?1",
         [hash],
         |row| row.get(0),
     );
@@ -89,5 +114,60 @@ pub fn insert_into(
             "More than one row was updated on insert: path={path}, hash={hash}"
         )));
     }
+    Ok(())
+}
+
+pub fn update_path(
+    transaction: &Transaction<'_>,
+    path: &Utf8Path,
+    hash: &str,
+) -> Result<(), Error> {
+    let mut query = transaction
+        .prepare("SELECT path FROM files as f where f.hash = ?1")
+        .map_err(Error::QueryFailure)?;
+    let matched_paths: Vec<String> = query
+        .query_map([hash], |row| row.get(0))
+        .map_err(Error::QueryFailure)?
+        .collect::<Result<_, _>>()
+        .map_err(Error::QueryFailure)?;
+    match matched_paths.len() {
+        0 => return Err(Error::HashDoesNotExist(hash.to_owned())),
+        1 => {}
+        2.. => {
+            return Err(Error::DuplicatePaths(
+                matched_paths.iter().map(Utf8PathBuf::from).collect(),
+            ))
+        }
+    };
+
+    let rows = transaction
+        .execute(
+            "UPDATE files
+             SET path = ?1
+             WHERE hash = ?2",
+            [path.as_str(), hash],
+        )
+        .map_err(Error::UpdateFailure)?;
+
+    match rows {
+        0 => {
+            return Err(Error::TooFewRowsAffected {
+                count: rows,
+                min_rows: 1,
+                max_rows: 1,
+                msg: "updating a hash path should update a single row".to_owned(),
+            })
+        }
+        1 => {}
+        2.. => {
+            return Err(Error::TooManyRowsAffected {
+                count: rows,
+                min_rows: 1,
+                max_rows: 1,
+                msg: "updating a hash path should update a single row".to_owned(),
+            })
+        }
+    };
+
     Ok(())
 }
--- src/init.rs
+++ src/init.rs
@@ -7,9 +7,10 @@ use crossterm::{
     cursor::{MoveToColumn, MoveUp},
     QueueableCommand,
 };
+use rusqlite::Transaction;
 
 use crate::db;
-use crate::utils::{hash_file, recursive_directory_read};
+use crate::utils::{self, hash_file, recursive_directory_read};
 
 pub fn init(data_path: &Utf8Path) -> Result<()> {
     let mut conn = db::open(data_path).wrap_err("Failed to open db")?;
@@ -39,11 +40,18 @@ pub fn init(data_path: &Utf8Path) -> Result<()> {
         println!("Adding file {i}/{total}...");
         stdout.flush().wrap_err("Failed flushing")?;
 
-        let h = hash_file(&p).wrap_err_with(|| format!("Could not hash file {p}"))?;
+        let h = hash_file(p).wrap_err_with(|| format!("Could not hash file {p}"))?;
         let p = p
             .strip_prefix(data_path)
             .wrap_err_with(|| format!("Path \"{p}\" was not a base of \"{data_path}\""))?;
-        db::insert_into(&transaction, p, &h).wrap_err("Failed inserting into table")?;
+        match db::insert_into(&transaction, p, &h) {
+            Ok(()) => {}
+            Err(db::Error::DuplicateInsertion { path_old, path_new }) => {
+                handle_duplicate(&transaction, data_path, &path_old, &path_new, &h)
+                    .wrap_err_with(|| format!("Could not handle duplicate file {p}"))?;
+            }
+            e @ Err(_) => e.wrap_err("Failed inserting into database")?,
+        }
     }
     transaction
         .commit()
@@ -51,5 +59,67 @@ pub fn init(data_path: &Utf8Path) -> Result<()> {
     let elapsed = now.elapsed();
     println!("Done generating database at \"{data_path}\". Took {elapsed:.2?}");
 
+    Ok(())
+}
+
+fn handle_duplicate(
+    transaction: &Transaction<'_>,
+    data_path: &Utf8Path,
+    path_old: &Utf8Path,
+    path_new: &Utf8Path,
+    hash: &str,
+) -> Result<()> {
+    const VALID_COMMANDS: &str = "Y/n/s/o/?";
+    let flush = || -> Result<()> { std::io::stdout().flush().wrap_err("Failed flushing stdout") };
+
+    print!("Found path \"{path_new}\", duplicate of \"{path_old}\", would you like to remove it? ({VALID_COMMANDS}): ");
+    flush()?;
+
+    let stdin = std::io::stdin();
+    loop {
+        let mut input = String::new();
+        stdin
+            .read_line(&mut input)
+            .wrap_err("Failed reading line from stdin")?;
+        println!();
+        flush()?;
+        match input.trim().to_lowercase().as_str() {
+            "" | "y" => {
+                let full_path = data_path.join(path_new);
+                utils::remove_file(&full_path)
+                    .wrap_err_with(|| format!("Could not remove path {path_new}"))?;
+                println!("Removed file {path_new}");
+                println!();
+                flush()?;
+                break;
+            }
+            "n" => {
+                println!("Quitting...");
+                std::process::exit(1);
+            }
+            "s" => todo!("Adding a file to the ignore list is not implemented"),
+            "o" => {
+                let full_path = data_path.join(path_old);
+                utils::remove_file(&full_path)
+                    .wrap_err_with(|| format!("Could not remove path {path_old}"))?;
+                println!("Removed file {path_old}");
+                db::update_path(transaction, path_new, hash)
+                    .wrap_err_with(|| format!("Could not update path {path_new} at {hash}"))?;
+                println!("Updated index with {path_new}");
+                println!();
+                flush()?;
+                break;
+            }
+            "?" => {
+                println!("y(Yes)  - Remove the new file");
+                println!("n(No)   - Do not remove the file and quit the program");
+                println!("s(Skip) - Skip the file and add it to the ignorelist");
+                println!("o(Old)  - Remove the old file and keep the new one");
+                println!("?(Help) - Print this message");
+            }
+            _ => println!("Invalid command, valid ones are ({VALID_COMMANDS})"),
+        }
+        flush()?;
+    }
     Ok(())
 }
